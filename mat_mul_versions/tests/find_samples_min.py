afd8364f9c6b678b3baedf5f0d2742361061ce29
Roberto Bruno <45689820+Rob11001@users.noreply.github.com>
2022-05-23 19:27:01 +0000

    Find opt script

---
+++ mat_mul_versions/tests/find_samples_min.py
@@ -0,0 +1,33 @@
+import csv
+import sys
+from os import listdir
+from os.path import isfile, join
+
+from requests import delete
+
+devices = ["CPU", "GPU"]
+
+for device in devices:
+    samplefiles = [f for f in listdir("./{}/samples/".format(device)) if isfile(join("./{}/samples".format(device), f))]
+
+    for file in samplefiles:
+        filepath = "./{0}/samples/{1}".format(device, file)
+        with open(filepath, mode="r") as input, \
+            open("./{0}/samples/opt/{1}".format(device, file), mode="w") as output:
+
+            reader = csv.DictReader(input)
+            fieldnames = reader.fieldnames
+            fieldnames = [field for field in fieldnames if field != "Timestamp"]
+            writer = csv.DictWriter(output, fieldnames=fieldnames)
+            writer.writeheader()
+
+            opt = sys.maxsize
+            optline = ""
+            for line in reader:
+                time = line["Time"]
+                time = float(time)
+                if time < opt:
+                    opt = time
+                    optline = line
+            optline.pop("Timestamp", None)
+            writer.writerow(optline)
